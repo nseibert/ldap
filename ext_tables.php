eeca7ecca08d6e946e424c20c8c0f0d7fc532902
Norman Seibert <seibert@entios.de>
2016-11-27 12:36:45 +0100

    Use new SVG extension icon

--- ext_tables.php
+++ ext_tables.php
@@ -17,11 +17,21 @@ if (TYPO3_MODE == "BE")    {
 			),
 			array(
 				'access' => 'user,group',
-				'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/mod_icon.gif',
+				'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ldap.svg',
 				'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml',
 			)
 		);
 	}
+
+	/**
+     * Register icons
+     */
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'extension-ldap-main',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:ldap/Resources/Public/Icons/ldap.svg']
+    );
 }
 
 $tempColumns = array(
