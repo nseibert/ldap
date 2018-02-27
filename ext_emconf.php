e9036c5fff649a6568688eaba091d062538b3758
Norman Seibert <seibert@entios.de>
2018-02-27 16:33:44 +0100

    Set correct TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'LDAP Integration',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '3.2.11',
+	'version' => '3.2.12',
 	'dependencies' => 'extbase,fluid',
 	'modify_tables' => 'fe_users,fe_groups,be_users,be_groups',
 	'conflicts' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '7.0.0-7.6.99',
+			'typo3' => '7.0.0-8.9.99',
 		),
 		'conflicts' => array(
 		),
