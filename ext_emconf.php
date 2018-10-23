b75eacb5bc48f7628f2c316069dfe37560d7a0cc
Norman Seibert <seibert@entios.de>
2018-10-23 18:48:46 +0200

    Update version and compatibility information

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'LDAP Integration',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '3.2.12',
+	'version' => '3.3.0',
 	'dependencies' => 'extbase,fluid',
 	'modify_tables' => 'fe_users,fe_groups,be_users,be_groups',
 	'conflicts' => '',
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-0.0.0',
-			'typo3' => '7.0.0-8.9.99',
+			'php' => '7.2.0-0.0.0',
+			'typo3' => '9.0.0-9.9.99',
 		),
 		'conflicts' => array(
 		),
