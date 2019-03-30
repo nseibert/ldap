6ca7e0b1b16af65800a6e2dddf6c81e28acc8967
Norman Seibert <seibert@entios.de>
2019-03-30 21:00:23 +0100

    Clean up ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,36 +1,31 @@
 <?php
-if (!defined("TYPO3_MODE")) {
-	exit("Access denied.");
-}
+defined('TYPO3_MODE') or die();
 
-if (TYPO3_MODE == "BE")    {
-	if (TYPO3_MODE === 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-			'NormanSeibert.' . $_EXTKEY,
-			'tools',	 // Make module a submodule of 'web'
-			'm1',	 // Submodule key
-			'',		 // Position
-			array(
-				'Module' => 'check, summary, importUsers, doImportUsers, updateUsers,
-							doUpdateUsers, importAndUpdateUsers, doImportAndUpdateUsers,
-							deleteUsers, doDeleteUsers, checkLogin, doCheckLogin'
-			),
-			array(
-				'access' => 'user,group',
-				'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ldap.svg',
-				'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml',
-			)
-		);
-	}
+// Register the backend module
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+	'NormanSeibert.' . $_EXTKEY,
+	'tools',	 // Make module a submodule of 'web'
+	'm1',	 // Submodule key
+	'',		 // Position
+	array(
+		'Module' => 'check, summary, importUsers, doImportUsers, updateUsers,
+			doUpdateUsers, importAndUpdateUsers, doImportAndUpdateUsers,
+			deleteUsers, doDeleteUsers, checkLogin, doCheckLogin'
+	),
+	array(
+		'access' => 'user,group',
+		'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ldap.svg',
+		'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xml',
+	)
+);
 
-	/**
-     * Register icons
-     */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'extension-ldap-main',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:ldap/Resources/Public/Icons/ldap.svg']
-    );
-}
+/**
+ * Register icons
+ */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+    'extension-ldap-main',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:ldap/Resources/Public/Icons/ldap.svg']
+);
 ?>
